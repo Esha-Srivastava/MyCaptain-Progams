6d9d8720f42d1d5f697b0d47362e2422670536ca
Esha Srivastava <66724873+Esha-Srivastava@users.noreply.github.com>
2020-06-23 19:21:11 +0530

    Created Switchcase.java
    to learn use of switch case in java
    Created on 23-06-2020

---
+++ Switchcase.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CopyProg 
+{
+	public static void main(String[] args)
+	{
+			int choice;
+			System.out.println("Pick one : \t1. Hi\t2. Hey\t3. Hello\t" );
+			Scanner s = new Scanner(System.in);
+			choice = s.nextInt();
+			switch(choice)
+		{
+			case 1 : System.out.println("You said Hi");
+			break;
+			case 2 : System.out.println("You said Hey");
+			break;
+			case 3 : System.out.println("You said Hello");
+			break;
+			default : System.out.println("Invalid choice");
+		}
+	}
+}
+
